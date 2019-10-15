0dcd5d0eee99ee45594920f69941deb094a08323
Baudin <ckelkboom@protonmail.com>
2019-10-15 14:40:30 +0200

    Initial commit

---
+++ COmpilerTests/LexerTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace COmpilerTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(2, 2);
+
+        }
+    }
+}
