12caf4e019e586eb538a0f014a16be994edb5bc0
lucashorward <lucas.horward@gmail.com>
2019-11-04 17:24:31 +0100

    Fixed paths

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@ rm -rf lib/*
 RELEASE_TARGETS=("win-x64" "osx-x64")
 for target in "${RELEASE_TARGETS[@]}"
 do
-    dotnet publish CLI -c Release --runtime "$target" /p:PublishSingleFile=true -o ../lib
+    dotnet publish CLI -c Release --runtime "$target" /p:PublishSingleFile=true -o lib
 done
-mv ../lib/CLI ../lib/ckc
-mv ../lib/CLI.exe ../lib/ckc.exe
+mv lib/CLI lib/ckc
+mv lib/CLI.exe lib/ckc.exe
