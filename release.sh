e694e97b3ef2e47e89a0466b9d707a544e4d3a56
lucashorward <lucas.horward@gmail.com>
2019-11-04 17:40:02 +0100

    Remove PDB from assets with a weird workaround

--- release.sh
+++ release.sh
@@ -3,7 +3,8 @@ rm -rf lib/*
 RELEASE_TARGETS=("win-x64" "osx-x64")
 for target in "${RELEASE_TARGETS[@]}"
 do
-    dotnet publish CLI -c Release --runtime "$target" /p:PublishSingleFile=true -o lib
+    dotnet publish CLI -c Release --runtime "$target" /p:PublishSingleFile=true -o releaseTemp
 done
-mv lib/CLI lib/ckc
-mv lib/CLI.exe lib/ckc.exe
+mv temp/CLI lib/ckc
+mv temp/CLI.exe lib/ckc.exe
+rm -rf temp/
