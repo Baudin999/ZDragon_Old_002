e025a242270bc746ae4c4d39d7920781e654b166
Baudin <ckelkboom@protonmail.com>
2019-11-04 20:19:42 +0100

    initial commit for the JSON Schema implementation

---
+++ CompilerTests/JSON/JsonTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xunit;
+using Mapper.JSON;
+using Compiler;
+using System.Linq;
+
+namespace CompilerTests.JSON
+{
+    public class JsonTests
+    {
+        [Fact]
+        public void TestJson()
+        {
+            var code = @"
+alias Name = String
+    & min 5
+    & max 28
+
+alias Names = List Name
+
+type Person =
+    @ The First Name of the Person
+    FirstName: Name;
+    LastName: Maybe String;
+    Age: Number;
+    Tags: List String
+        & min 3
+        & max 30
+    ;
+    Names: Names;
+
+";
+            var tokens = new Lexer().Lex(code);
+            var parser = new Parser(tokens);
+            var parseTree = parser.Parse().ToList();
+
+            JsonMapper c = new JsonMapper(parseTree);
+        }
+    }
+    
+}
