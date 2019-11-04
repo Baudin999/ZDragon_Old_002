e9aa8540a5c7da594608e28e00f3a821107ff36a
lucashorward <lucas.horward@gmail.com>
2019-11-04 17:19:35 +0100

    Improved travis

--- release.sh
+++ release.sh
@@ -1,6 +1,9 @@
 ﻿#!/bin/bash
+rm -rf lib/*
 RELEASE_TARGETS=("win-x64" "osx-x64")
 for target in "${RELEASE_TARGETS[@]}"
 do
-    dotnet publish CLI -c Release --runtime "$target" /p:PublishSingleFile=true
+    dotnet publish CLI -c Release --runtime "$target" /p:PublishSingleFile=true -o ../lib
 done
+mv ../lib/CLI ../lib/ckc
+mv ../lib/CLI.exe ../lib/ckc.exe
