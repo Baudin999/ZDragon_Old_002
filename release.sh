e4f8a8a37bf8e1b1dddf7f877655c8944dd79be8
lucashorward <lucas.horward@gmail.com>
2019-11-05 16:52:22 +0100

    Cleanup & comments

--- release.sh
+++ release.sh
@@ -1,19 +1,22 @@
 ﻿#!/bin/bash
 rm -rf lib/
+# Build for the wanted platforms and place it in releaseTemp
 RELEASE_TARGETS=("win-x64" "osx-x64")
 for target in "${RELEASE_TARGETS[@]}"
 do
     dotnet publish CLI -c Release --runtime "$target" /p:PublishSingleFile=true -o releaseTemp
 done
+# Move from releaseTemp to lib, this way unneeded files (such as .pdb files) are omitted from the release
 mkdir lib
 mv releaseTemp/CLI lib/ckc
 mv releaseTemp/CLI.exe lib/ckc.exe
 rm -rf releaseTemp/
+# Get the previous tag from git history
 PREVIOUSTAG=$(git describe --abbrev=0 --tags $(git rev-list --tags --skip=1 --max-count=1))
+# Get the newest tag through parameters (see .travis.yml)
 NEWTAG=$1
-# TODO change all project versions at the same time
-echo $PREVIOUSTAG
-echo $NEWTAG
+# Loop through files where the old version has to be replaced with the new version
+# Technically, the paths to csproj files could be read out from the .sln, but this would make adding custom files (such as the README) more difficult
 FILES=("ZDragon.NET.sln" "CLI/CLI.csproj" "Compiler/Compiler.csproj" "CompilerTests/CompilerTests.csproj" "Mapper.xsd/Mapper.xsd.csproj")
 for FILE in "${FILES[@]}"
 do
