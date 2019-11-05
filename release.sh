f9c60dbfa0d3ef04eb2eadeba45d04bf2b60ca95
lucashorward <lucas.horward@gmail.com>
2019-11-05 16:36:56 +0100

    Update files with commits

--- release.sh
+++ release.sh
@@ -9,3 +9,11 @@ mkdir lib
 mv releaseTemp/CLI lib/ckc
 mv releaseTemp/CLI.exe lib/ckc.exe
 rm -rf releaseTemp/
+PREVIOUSTAG=$(git describe --abbrev=0 --tags)
+NEWTAG=$1
+# TODO change all project versions at the same time
+FILES=("ZDragon.NET.sln" "CLI/CLI.csproj" "Compiler/Compiler.csproj" "CompilerTests/CompilerTests.csproj" "Mapper.xsd/Mapper.xsd.csproj")
+for FILE in "${FILES[@]}"
+do
+    sed -i '' -e "s/$PREVIOUSTAG/$NEWTAG/g" $FILE
+done
