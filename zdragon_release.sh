6219d443367b1972ac2ca025646f5d1366a7db08
Lucas Horward <lucas.horward@gmail.com>
2019-11-13 14:44:09 +0100

    Added easy dev release script

---
+++ zdragon_release.sh
@@ -0,0 +1,10 @@
+# The following variables are defaults, but can be overwritten as cli arguments, such as by running zdragon_release /usr/local/tempBinForDev
+# This can be useful for one-time changes, but it is recommended to just edit the file if you want to do this often.
+
+# Default bin location for mac
+OUTPUT_DIR="${1:-/usr/local/bin}"
+# Defaults to mac
+OS="${2:-osx-x64}"
+# If your zdragon source location differs from this, edit this variable.
+SOURCE_DIR="${3:-$HOME/projects/ZDragon.NET/CLI}"
+dotnet publish "$SOURCE_DIR" -c Release --runtime "$OS" /p:PublishSingleFile=true -o "$OUTPUT_DIR"
