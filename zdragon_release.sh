eddcc0eba00d394d1c608a044d20d3eee56bb720
lucashorward <lucas.horward@gmail.com>
2019-11-13 15:11:41 +0100

    Renamed zdragon to ckc

--- zdragon_release.sh
+++ zdragon_release.sh
@@ -2,9 +2,11 @@
 # This can be useful for one-time changes, but it is recommended to just edit the file if you want to do this often.
 
 # Default bin location for mac
-OUTPUT_DIR="${1:-/usr/local/bin}"
+OUTPUT_DIR="${1:-/usr/local/bin/}"
 # Defaults to mac
 OS="${2:-osx-x64}"
 # If your zdragon source location differs from this, edit this variable.
 SOURCE_DIR="${3:-$HOME/projects/ZDragon.NET/CLI}"
+APP_NAME="${4:-ckc}"
 dotnet publish "$SOURCE_DIR" -c Release --runtime "$OS" /p:PublishSingleFile=true -o "$OUTPUT_DIR"
+mv -v "$OUTPUT_DIR"CLI "$OUTPUT_DIR""$APP_NAME"
