0fd8e7c81a821e0f7a74c46329f4bf505ef3b5b4
lucashorward <lucas.horward@gmail.com>
2019-11-24 12:05:02 +0100

    Removed untestable master stuff

--- localDeployment.sh
+++ localDeployment.sh
@@ -3,7 +3,6 @@ NEWTAG=$1
 EXISTINGTAG=$2
 if [ -z "$(git status --porcelain)" ]; then 
 	# clean working dir
-	if [ $(git rev-parse --abbrev-ref HEAD) -eq $"master" ]; then
 		FILES=("ZDragon.NET.sln" "CLI/CLI.csproj" "Compiler/Compiler.csproj" "CompilerTests/CompilerTests.csproj" "Mapper.XSD/Mapper.XSD.csproj" "README.md"
 		"Mapper.HTML/Mapper.HTML.csproj" "Mapper.JSON/Mapper.JSON.csproj"
 		"CLI/Program.cs"
@@ -12,10 +11,6 @@ if [ -z "$(git status --porcelain)" ]; then
 		do
 			sed -i '' -e "s/$EXISTINGTAG/$NEWTAG/g" $FILE
 		done
-	else
-		echo "You are not on the master branch. Aborting."
-		exit 1
-	fi
 else 
 	# uncommited changes
 	echo "You have uncommited changes. Stash, commit or remove them. Aborting."
