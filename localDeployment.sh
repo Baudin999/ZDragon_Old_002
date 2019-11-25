c9ff43d0a049adb1c084a333ee066d2257e4f574
lucashorward <lucas.horward@gmail.com>
2019-11-25 09:26:02 +0100

    Made existing tag optional arg

--- localDeployment.sh
+++ localDeployment.sh
@@ -1,20 +1,22 @@
 NEWTAG=$1
-EXISTINGTAG=$(git describe --tag --abbrev=0)
+EXISTINGTAG="${2:-$(git describe --tag --abbrev=0)}"
+# "${1:-/usr/local/bin/}"
+echo $EXISTINGTAG
 if [ -z "$(git status --porcelain)" ]; then 
 	# clean working dir
 		FILES=("ZDragon.NET.sln" "CLI/CLI.csproj" "Compiler/Compiler.csproj" "CompilerTests/CompilerTests.csproj" "Mapper.XSD/Mapper.XSD.csproj" "README.md"
 		"Mapper.HTML/Mapper.HTML.csproj" "Mapper.JSON/Mapper.JSON.csproj"
 		"CLI/Program.cs"
 		)
-		git checkout master
+		#git checkout master
 
 		for FILE in "${FILES[@]}"
 		do
 			sed -i '' -e "s/$EXISTINGTAG/$NEWTAG/g" $FILE
 		done
-		git add .
-		git commit -m "Release $NEWTAG"
-		git push origin master
+		#git add .
+		#git commit -m "Release $NEWTAG"
+		#git push origin master
 else 
 	# uncommited changes
 	echo "You have uncommited changes. Stash, commit or remove them. Aborting."
