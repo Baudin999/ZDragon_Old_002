36c91e8314849327130d71b9f710455df4944fae
Lucas Horward <lucas.horward@gmail.com>
2019-11-29 21:41:10 +0100

    Improved github release with additional check

--- github_release.sh
+++ github_release.sh
@@ -1,17 +1,18 @@
 # Checkout first to get correct tag
  git checkout master 
  EXISTINGTAG="${2:-$(git describe --tag --abbrev=0)}"
- if [ -z "$(git status --porcelain)" ]; then 
+ git fetch
+ HEADHASH=$(git rev-parse HEAD)
+ UPSTREAMHASH=$(git rev-parse master@{upstream})
+ if [ -z "$(git status --porcelain)" ] && [ "$HEADHASH" == "$UPSTREAMHASH" ]; then 
      # clean working dir
-     read -p "This will replace $EXISTINGTAG with "$1" . Are you sure? " -n 1 -r
-     if [[ $REPLY =~ ^[Yy]$ ]]
-     then
+     read -p "This will replace $EXISTINGTAG with "$1". Are you sure? (y/n)" -n 1 -r
+     if [[ $REPLY =~ ^[Yy]$ ]]; then
          FILES=("ZDragon.NET.sln" "CLI/CLI.csproj" "Compiler/Compiler.csproj" "CompilerTests/CompilerTests.csproj" "Mapper.XSD/Mapper.XSD.csproj" "README.md"
          "Mapper.HTML/Mapper.HTML.csproj" "Mapper.JSON/Mapper.JSON.csproj"
-         "CLI/Program.cs" "docs/index.md"
-         )
+         "CLI/Program.cs" "docs/index.md")
 
-          for FILE in "${FILES[@]}"
+         for FILE in "${FILES[@]}"
          do
              sed -i '' "s/$EXISTINGTAG/$1/g" $FILE
          done
@@ -20,11 +21,11 @@
          git tag "$1"
          git push origin master --tags 
      else
-         echo "Exiting"
+         echo "Exiting (no changes made)."
          exit 0
      fi
  else 
      # uncommited changes
-     echo "You have uncommited changes. Stash, commit or remove them. Aborting."
+     echo "You have uncommited or unpushed/pulled changes. Stash, commit or remove them. Exiting (no changes made)."
      exit 1
  fi 
