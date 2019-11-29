8e8aa7cc1704156196f25c3ab7e22704aae91bfc
Lucas Horward <lucas.horward@gmail.com>
2019-11-29 21:27:50 +0100

    Added bash profile question, made folder check more specific

--- local_release.sh
+++ local_release.sh
@@ -1,19 +1,23 @@
 #!/bin/bash -i
-
-# Default bin location for mac
 if [ -z ${ZDRAGON_PATH} ]; then
     read -p "Please enter your ZDragon.NET source path: `echo $'\n'`" -e
-    if [ ! $(ls -a $REPLY | grep .sln) ]; then
-        echo "This is not a valid dotnet project"
+    if [ ! $(ls -a $REPLY | grep .sln) ] || [ ! $(ls -a $REPLY | grep CLI) ] ; then
+        echo "This is not a valid dotnet project, or it does not contain the ZDragon.NET project. Exiting."
         exit 1;
     fi
-    echo -e "export ZDRAGON_PATH=$REPLY\n" >> $HOME/.bash_profile
+    PATH = $REPLY
+    read -p "Would you like to add this path to your bash profile to be re-used in the next start? (y/n)" -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        echo -e "export ZDRAGON_PATH=$PATH\n" >> $HOME/.bash_profile
+        source ~/.bash_profile
+        echo "Added $PATH to your bash profile."
+    fi
 fi
 
 OUTPUT_DIR="${1:-/usr/local/bin/}"
 # Defaults to mac
 OS="${2:-osx-x64}"
-# If your zdragon source location differs from this, edit this variable.
+# assumes that the path ends with /
 CLI_PATH=${ZDRAGON_PATH}CLI
 SOURCE_DIR="${3:-$CLI_PATH}"
 APP_NAME="${4:-ckc}"
