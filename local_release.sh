dfa56e240421e54917107e2fd08d935a6242a66d
lucashorward <lucas.horward@gmail.com>
2019-11-29 13:49:06 +0100

    Improved local release

--- local_release.sh
+++ local_release.sh
@@ -1,12 +1,21 @@
-# The following variables are defaults, but can be overwritten as cli arguments, such as by running zdragon_release /usr/local/tempBinForDev
-# This can be useful for one-time changes, but it is recommended to just edit the file if you want to do this often.
+#!/bin/bash -i
 
 # Default bin location for mac
+if [ -z ${ZDRAGON_PATH} ]; then
+    read -p "Please enter your ZDragon.NET source path: `echo $'\n'`" -e
+    if [ ! $(ls -a $REPLY | grep .sln) ]; then
+        echo "This is not a valid dotnet project"
+        exit 1;
+    fi
+    echo -e "export ZDRAGON_PATH=$REPLY\n" >> $HOME/.bash_profile
+fi
+
 OUTPUT_DIR="${1:-/usr/local/bin/}"
 # Defaults to mac
 OS="${2:-osx-x64}"
 # If your zdragon source location differs from this, edit this variable.
-SOURCE_DIR="${3:-$HOME/Projects/dotnet/ZDragon.NET/CLI}"
+CLI_PATH=${ZDRAGON_PATH}CLI
+SOURCE_DIR="${3:-$CLI_PATH}"
 APP_NAME="${4:-ckc}"
 dotnet publish "$SOURCE_DIR" -c Release --runtime "$OS" /p:PublishSingleFile=true -o "$OUTPUT_DIR"
 mv -v "$OUTPUT_DIR"CLI "$OUTPUT_DIR""$APP_NAME"
