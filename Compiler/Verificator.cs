0a87bd04dde8e0085db6423d1053ed4c0f2dab9b
Baudin <ckelkboom@protonmail.com>
2019-11-30 16:32:06 +0100

    Fixed a bug in the unit tests

--- Compiler/Verificator.cs
+++ Compiler/Verificator.cs
@@ -61,7 +61,7 @@ namespace Compiler
                         };
 
                         var baseTypes = new[] { "String", "Number", "Boolean", "Date", "Time", "DateTime" };
-                        if (!baseTypes.Contains(__type))
+                        if (!baseTypes.Contains(__type) && !__type.StartsWith("'", StringComparison.CurrentCulture))
                         {
                             var foundNode = ParseTree.FirstOrDefault(n => n is INamable && ((INamable)n).Name == _type);
                             if (foundNode is null)
