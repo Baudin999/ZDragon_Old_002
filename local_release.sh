ef523967baa262d262be48b626358ba1a6371db9
lucashorward <lucas.horward@gmail.com>
2019-12-02 10:25:31 +0100

    Fixed syntax error on assign and non-functional source command in local

--- local_release.sh
+++ local_release.sh
@@ -1,16 +1,17 @@
 #!/bin/bash -i
+RED='\033[0;31m'
+NOCOLOR='\033[0m'
 if [ -z ${ZDRAGON_PATH} ]; then
     read -p "Please enter your ZDragon.NET source path (folder with the dotnet solution): `echo $'\n'`" -e
     if [ ! $(ls -a $REPLY | grep .sln) ] || [ ! $(ls -a $REPLY | grep CLI) ] ; then
         echo "This is not a valid dotnet solution, or it does not contain the ZDragon.NET solution. Exiting."
         exit 1;
     fi
-    ZDRAGON_PATH = $REPLY
-    read -p "Would you like to add this path to your bash profile to be re-used in the next start? (y/n)" -n 1 -r
+    ZDRAGON_PATH=$REPLY
+    read -p "Would you like to add this path to your bash profile to be re-used in the next start? (y/n) `echo $'\n'`" -n 1 -r -e
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         echo -e "export ZDRAGON_PATH=$ZDRAGON_PATH\n" >> $HOME/.bash_profile
-        source ~/.bash_profile
-        echo "Added $ZDRAGON_PATH to your bash profile."
+        echo -e "${RED}Please run 'source ~/.bash_profile'. This will prevent you from needing to re-do this the next run.${NOCOLOR}"
     fi
 fi
 
