f4a801a62eb2908d67f5c42b2a27b5e57e4f4981
Baudin <ckelkboom@protonmail.com>
2019-12-04 10:04:11 +0100

    Release v2.0.8-beta

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -23,7 +23,7 @@ To quit the application press 'q'
             var app = new CommandLineApplication();
             app.Name = "ckc";
             app.HelpOption("-?|-h|--help");
-            app.VersionOption("-v|--version", "v2.0.7-beta");
+            app.VersionOption("-v|--version", "v2.0.8-beta");
 
             app.OnExecute(() => 0);
 
