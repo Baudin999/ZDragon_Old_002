52455bbad56d92c250bd50acd8f221567c824d9c
Baudin <ckelkboom@protonmail.com>
2019-12-05 15:25:17 +0100

    Release v2.1.2-beta

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -29,7 +29,7 @@ To quit the application press 'q'
             var app = new CommandLineApplication();
             app.Name = "ckc";
             app.HelpOption("-?|-h|--help");
-            app.VersionOption("-v|--version", "v2.1.1-beta");
+            app.VersionOption("-v|--version", "v2.1.2-beta");
 
             app.OnExecute(() => 0);
 
--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 # ZDragon.NET
 ZDragon is moving to .NET!
 
-# Current Version: v2.1.1-beta
+# Current Version: v2.1.2-beta
