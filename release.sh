8296d60bdde58d571bef2871c701e5a3ae8194af
Lucas Horward <l.horward@essent.nl>
2019-12-20 17:10:49 +0100

    Added linux to release

--- release.sh
+++ release.sh
@@ -9,28 +9,10 @@ do
 done
 
 # Move from releaseTemp to lib, this way unneeded files (such as .pdb files) are omitted from the release
+# Because dotnet Publish cannot give specific file, linux is seperate. Both output "CLI" executable.
+dotnet publish CLI -c Release --runtime linux-musl-x64 /p:PublishSingleFile=true -o releaseLinux
 mkdir lib
 mv releaseTemp/CLI lib/ckc
 mv releaseTemp/CLI.exe lib/ckc.exe
-rm -rf releaseTemp/
-
-## Get the previous tag from git history
-#PREVIOUSTAG=$(git describe --abbrev=0 --tags $(git rev-list --tags --skip=1 --max-count=1))
-## Get the newest tag through parameters (see .travis.yml)
-#NEWTAG=$1
-## Loop through files where the old version has to be replaced with the new version
-## Technically, the paths to csproj files could be read out from the .sln, but this would make adding custom files (such as the README) more difficult
-#FILES=("ZDragon.NET.sln" "CLI/CLI.csproj" "Compiler/Compiler.csproj" "CompilerTests/CompilerTests.csproj" "Mapper.xsd/Mapper.xsd.csproj" "README.md" "Mapper.HTML/Mapper.HTML.csproj" "Mapper.JSON/Mapper.JSON.csproj")
-#for FILE in "${FILES[@]}"
-#do
-#    sed -i '' -e "s/$PREVIOUSTAG/$NEWTAG/g" $FILE
-#done
-#
-#git config user.email "travis@travis-ci.org"
-#git config user.name "Travis CI"
-#git remote rm origin
-#git remote add origin https://Baudin999:"$2"@github.com/Baudin999/ZDragon.NET.git
-#git add .
-#git commit --message "Travis Release: $NEWTAG"
-## Use quiet to prevent leaking of tokens
-#git push --quiet origin master
+mv releaseLinux/CLI lib/ckcl
+rm -rf releaseTemp/ releaseLinux
